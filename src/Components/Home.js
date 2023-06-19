61d0762e7ab218c589e6e639470ecdbe3f12e7a2
MuskanSrivastava8 <muskansrivastava8756@gmail.com>
2023-06-19 11:40:17 +0530

    commit_know_your_show added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,7 @@ import Portfolio from "./Portfolio/Portfolio";
 import Help from "./Help";
 import Menu from "./Menu";
 import Intro from "./Intro/Intro";
+import ProjectOneComponent from "./ProjectOne/ProjectOneComponent";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
         <Route exact path="/" element={<Intro />} />
         <Route exact path="/Intro" element={<Intro />} />
         <Route exact path="/Portfolio" element={<Portfolio />} />
-        <Route path="/Knowyourshow" element={<Knowyourshow />} />
+        <Route path="/Knowyourshow" element={<ProjectOneComponent />} />
         <Route path="*" element={<Help />} />
       </Routes>
     </React.Fragment>
--- src/Components/Knowyourshow.js
+++ src/Components/Knowyourshow.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ProjectOneComponent from './ProjectOne/ProjectOneComponent'
 
 export default function Knowyourshow() {
   return (
     <div>
       Knowyourshow page
+      <ProjectOneComponent/>
     </div>
   )
 }
