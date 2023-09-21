362864c2c1c432446e887b9ec5e7359283693aea
MuskanSrivastava8 <muskansrivastava8756@gmail.com>
2023-09-22 00:05:31 +0530

    Portfolio view toggle feature added

--- src/Components/Portfolio/Summary.js
+++ src/Components/Portfolio/Summary.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.scss";
 import { AiOutlineLaptop } from "react-icons/ai";
 import { BsFillEnvelopeAtFill } from "react-icons/bs";
@@ -8,94 +8,273 @@ import { BsTrophy } from "react-icons/bs";
 import { BsPencilSquare } from "react-icons/bs";
 
 export default function Summary() {
+  const [showCordianView, setshowCordianView] = useState(true);
+  const [cardView, setcardView] = useState("");
+
+  // const [cordianView, setcordianView] = useState("");
+
+
+  const toggleView = () => {
+    setshowCordianView(!showCordianView);
+    // if(showCordianView){
+    //   setcardView(true)
+    // }
+  };
   return (
     <React.Fragment>
-      <div className="summary_container">
-        <div className="row_1">
-          <div className="summary_box">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div className="card_text">
-                    <div className="card_heading">Skill</div>
-                    <AiOutlineLaptop />
+      <button onClick={toggleView}>{showCordianView ? "List View" : "Card View"}</button>
+      
+      {showCordianView ? (
+        <div className="summary_container">
+          <div className="row_1">
+            <div className="summary_box">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <div className="card_text">
+                      <div className="card_heading">Skill</div>
+                      <AiOutlineLaptop />
+                    </div>
+                  </div>
+                  <div class="flip-card-back">
+                    <div className="points_heading">
+                      <h1>Skill</h1>
+                    </div>
+                    <div className="points_details">
+                      <p>
+                        React, Javascript, Typescript, Redux, Sass, HTML, CSS
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <div class="flip-card-back">
-                  <div className="points_heading">
-                    <h1>Skill</h1>
+              </div>
+            </div>
+            <div className="summary_box">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <div className="card_text">
+                      <div className="card_heading">Project</div>
+                      <BsCodeSlash />
+                    </div>
                   </div>
-                  <div className="points_details">
-                  <p>React, Javascript, Typescript, Redux, Sass, HTML, CSS</p>
+                  <div class="flip-card-back">
+                    <div className="points_heading">
+                      <h5>Project Experience (TCS)</h5>
+                    </div>
+                    <div className="points_details">
+                      <p>Medline : React Developer</p>
+                      <p>E2open : Application Developer</p>
+                    </div>
+                    <div className="points_heading">
+                      <h5>Personal Project</h5>
+                    </div>
+                    <div className="points_details">
+                      <p>Mitra</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="summary_box">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <div className="card_text">
+                      <div className="card_heading">Activities</div>
+                      <BsPencilSquare />
+                    </div>
+                  </div>
+                  <div class="flip-card-back">
+                    <div className="points_heading">
+                      <h1>Activities</h1>
+                    </div>
+                    <div className="points_details">
+                      <p>T-factor : 1.8</p>
+                      <p>iEvolve score : 1320</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="summary_box">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div className="card_text">
-                    <div className="card_heading">Project</div>
-                    <BsCodeSlash />
+          <div className="row_2">
+            <div className="summary_box">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <div className="card_text">
+                      <div className="card_heading">Education</div>
+                      <BsFillMortarboardFill />
+                    </div>
                   </div>
-                </div>
-                <div class="flip-card-back">
-                  <div className="points_heading">
-                    <h5>Project Experience (TCS)</h5>
-                  </div>
-                  <div className="points_details">
-                  <p>Medline : React Developer</p>
-                  <p>E2open : Application Developer</p>
-                  </div>
-                  <div className="points_heading">
-                    <h5>Personal Project</h5>
-                  </div>
-                  <div className="points_details">
-                  <p>Mitra</p>
+                  <div class="flip-card-back">
+                    <div className="points_heading">
+                      <h1>Education</h1>
+                    </div>
+
+                    <div>
+                      {
+                        <table className="center">
+                          <tr>
+                            <th>B.Tech</th>
+                            <td>84%</td>
+                          </tr>
+
+                          <tr>
+                            <th>12th</th>
+                            <td>83%</td>
+                          </tr>
+
+                          <tr>
+                            <th>10th</th>
+                            <td>83%</td>
+                          </tr>
+                        </table>
+                      }
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="summary_box">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div className="card_text">
-                    <div className="card_heading">Activities</div>
-                    <BsPencilSquare />
+            <div className="summary_box">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <div className="card_text">
+                      <div className="card_heading">Awards</div>
+                      <BsTrophy />
+                    </div>
+                  </div>
+                  <div class="flip-card-back">
+                    <div className="points_heading">
+                      <h1>Awards</h1>
+                    </div>
+                    <div className="points_details">
+                      <p>On the spot Awards</p>
+                      <p>Elevate wings Awards</p>
+                      <p>Xcelerate Warrior Certificate</p>
+                    </div>
                   </div>
                 </div>
-                <div class="flip-card-back">
-                  <div className="points_heading">
-                    <h1>Activities</h1>
+              </div>
+            </div>
+            <div className="summary_box">
+              <div class="flip-card">
+                <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                    <div className="card_text">
+                      <div className="card_heading">Contact</div>
+                      <BsFillEnvelopeAtFill />
+                    </div>
                   </div>
-                  <div className="points_details">
-                  <p>T-factor : 1.8</p>
-                  <p>iEvolve score : 1320</p>
+                  <div class="flip-card-back">
+                    <div className="points_heading">
+                      <h1>Contact</h1>
+                    </div>
+                    <div className="points_details">
+                      <p>875XXXXXX4</p>
+                      <p>muskan.srivastava@tcs.com</p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="row_2">
-          <div className="summary_box">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div className="card_text">
-                    <div className="card_heading">Education</div>
-                    <BsFillMortarboardFill />
-                  </div>
+      ) : (
+        <div>
+          <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                <button
+                  class="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseOne"
+                  aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseOne"
+                >
+                  Skill
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseOne"
+                class="accordion-collapse collapse show"
+                aria-labelledby="panelsStayOpen-headingOne"
+              >
+                <div class="accordion-body">
+                  React, Javascript, Typescript, Redux, Sass, HTML, CSS
                 </div>
-                <div class="flip-card-back">
-                  <div className="points_heading">
-                    <h1>Education</h1>
-                  </div>
-
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseTwo"
+                >
+                  Project
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseTwo"
+                class="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingTwo"
+              >
+                <div class="accordion-body">
+                  <p>Medline : React Developer</p>
+                  <p>E2open : Application Developer</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseThree"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseThree"
+                >
+                  Activities
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseThree"
+                class="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingThree"
+              >
+                <div class="accordion-body">
+                  <p>T-factor : 1.8</p>
+                  <p>iEvolve score : 1320</p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingFour">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseFour"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseFour"
+                >
+                  Education
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseFour"
+                class="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingFour"
+              >
+                <div class="accordion-body">
                   <div>
                     {
                       <table className="center">
@@ -119,52 +298,58 @@ export default function Summary() {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="summary_box">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div className="card_text">
-                    <div className="card_heading">Awards</div>
-                    <BsTrophy />
-                  </div>
-                </div>
-                <div class="flip-card-back">
-                  <div className="points_heading">
-                    <h1>Awards</h1>
-                  </div>
-                  <div className="points_details">
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingFive">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseFive"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseFive"
+                >
+                  Awards
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseFive"
+                class="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingFive"
+              >
+                <div class="accordion-body">
                   <p>On the spot Awards</p>
                   <p>Elevate wings Awards</p>
                   <p>Xcelerate Warrior Certificate</p>
-                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="summary_box">
-            <div class="flip-card">
-              <div class="flip-card-inner">
-                <div class="flip-card-front">
-                  <div className="card_text">
-                    <div className="card_heading">Contact</div>
-                    <BsFillEnvelopeAtFill />
-                  </div>
-                </div>
-                <div class="flip-card-back">
-                  <div className="points_heading">
-                    <h1>Contact</h1>
-                  </div>
-                  <div className="points_details">
-                    <p>875XXXXXX4</p>
-                    <p>muskan.srivastava@tcs.com</p>
-                  </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header" id="panelsStayOpen-headingSix">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseSix"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseSix"
+                >
+                  Contact
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseSix"
+                class="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingSix"
+              >
+                <div class="accordion-body">
+                  <p>875XXXXXX4</p>
+                  <p>muskan.srivastava@tcs.com</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </React.Fragment>
   );
 }
